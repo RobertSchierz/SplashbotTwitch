bdc6f34a2658b34ea422db3ef61ab24c107e4754
Jan <s65613@beuth-hochschule.de>
2016-06-07 00:38:15 +0200

    added pircbot framework

---
+++ src/bot/SplashBot.java
@@ -0,0 +1,13 @@
+package bot;
+
+import org.jibble.pircbot.PircBot;
+
+/**
+ * Created by Jan on 07.06.2016.
+ */
+public class SplashBot extends PircBot{
+
+    SplashBot(){
+        this.setName("SplashBot");
+    }
+}
