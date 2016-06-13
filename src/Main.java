09104b1de73aedb2532e3822208725b98875c7f3
Jan <s65613@beuth-hochschule.de>
2016-06-13 21:18:07 +0200

    !uptime command implementiert

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 
 public class Main {
 
-    public static String channel = "#robdebang";
-    public static String auth = "oauth:bat48hhqj1bebsju7eigtxbvxcw2p8";
-
     public static void main(String[] args) {
 
 
@@ -16,28 +13,8 @@ public class Main {
 
         // Enable debugging output.
         bot.setVerbose(true);
+        bot.connectToChannel();
 
-        // Connect to the IRC server.
-        try {
-            bot.connect("irc.twitch.tv", 80, auth);
-            //bot.connect("irc.twitch.tv", 6667, g_bot_oauth);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (IrcException e) {
-            e.printStackTrace();
-        }
-
-        // Join the #pircbot channel.
-        bot.joinChannel(channel);
-        bot.getName();
-
-        bot.sendMessage(channel, "hallo welt!");
-
-        bot.sendMessage(channel, "schwanz");
-
-        bot.sendMessage(channel, "crunch'a'tize me capt'n");
-
-
-        //oauth:bat48hhqj1bebsju7eigtxbvxcw2p8
+        bot.send("hallo welt!");
     }
 }
--- src/bot/SplashBot.java
+++ src/bot/SplashBot.java
@@ -1,13 +1,24 @@
 package bot;
 
+import org.jibble.pircbot.IrcException;
 import org.jibble.pircbot.PircBot;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Jan on 07.06.2016.
  */
-public class SplashBot extends PircBot{
+public class SplashBot extends PircBot {
 
-    public SplashBot(){
+    public String channel = "#splshbot";
+    //public String channel = "#splshbot";
+    public String auth = "oauth:bat48hhqj1bebsju7eigtxbvxcw2p8";
+
+    private Long startTimeMillis;
+
+    public SplashBot() {
         this.setName("splshbot");
         this.isConnected();
     }
@@ -15,7 +26,65 @@ public class SplashBot extends PircBot{
     @Override
     protected void onMessage(String channel, String sender, String login, String hostname, String message) {
         System.out.println(sender + ": " + message);
+        checkMessage(message);
     }
 
+    public void checkMessage(String message){
+        if (message.equalsIgnoreCase("!test")){
+            send(":D");
+        }
+        if (message.equalsIgnoreCase("!uptime")){
+            getUptime();
+        }
+    }
 
+    /**
+     * Connect to the IRC server and saves the current time for the 'uptime' command.
+     */
+    public void connectToChannel() {
+        try {
+            this.connect("irc.twitch.tv", 80, auth);
+            this.joinChannel(channel);
+        } catch (IOException | IrcException e) {
+            e.printStackTrace();
+        }
+
+        //save current time for 'uptime' command
+        if (this.isConnected()){
+            startTimeMillis = System.currentTimeMillis();
+        }
+    }
+
+    public void send(String message){
+        this.sendMessage(channel, message);
+    }
+
+    private void getUptime(){
+        Long uptimeMillis = System.currentTimeMillis();
+
+        Long elapsedTime = uptimeMillis - startTimeMillis;
+
+        long timeInSeconds = elapsedTime / 1000;
+        long hours, minutes, seconds;
+        hours = timeInSeconds / 3600;
+        timeInSeconds = timeInSeconds - (hours * 3600);
+        minutes = timeInSeconds / 60;
+        timeInSeconds = timeInSeconds - (minutes * 60);
+        seconds = timeInSeconds;
+
+        String diffTime =
+                (hours<10 ? "0" + hours : hours) + " h "
+                + (minutes < 10 ? "0" + minutes : minutes) + " min "
+                + (seconds < 10 ? "0" + seconds : seconds) + " sec";
+
+
+        send(diffTime);
+
+        //String uptime = startTimeMillis.toString();
+        //SimpleDateFormat sdf = new SimpleDateFormat("dd:hh:mm:ss");
+        //Date resultdate = new Date(elapsedTime);
+        //System.out.println("Startzeit: " + sdf.format(new Date(startTimeMillis)));
+        //System.out.println(sdf.format(new Date(uptimeMillis)));
+        //send(sdf.format(resultdate));
+    }
 }
