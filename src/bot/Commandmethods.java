d27c238212f8f1e6cb8b7b502a449a55a4c96bf9
Schierz, Robert <s65727@beuth-hochschule.de>
2016-07-04 23:40:25 +0200

    Funktionale Commands funktionieren jetzt

--- src/bot/Commandmethods.java
+++ src/bot/Commandmethods.java
@@ -4,8 +4,39 @@ package bot;
  * Created by Robert on 04.07.2016.
  */
 public class Commandmethods extends SplashBot {
+    SplashBot bot;
+    Commandmethods(SplashBot bot){
+       this.bot = bot;
+    }
 
     public void getUptime(){
-        System.out.println("Uptime Amk");
+
+        Long uptimeMillis = System.currentTimeMillis();
+
+        Long elapsedTime = uptimeMillis - this.bot.startTimeMillis;
+
+        long timeInSeconds = elapsedTime / 1000;
+        long hours, minutes, seconds;
+        hours = timeInSeconds / 3600;
+        timeInSeconds = timeInSeconds - (hours * 3600);
+        minutes = timeInSeconds / 60;
+        timeInSeconds = timeInSeconds - (minutes * 60);
+        seconds = timeInSeconds;
+
+        String diffTime =
+                (hours<10 ? "0" + hours : hours) + " h "
+                        + (minutes < 10 ? "0" + minutes : minutes) + " min "
+                        + (seconds < 10 ? "0" + seconds : seconds) + " sec";
+
+
+        this.bot.send(diffTime);
+
+        //String uptime = startTimeMillis.toString();
+        //SimpleDateFormat sdf = new SimpleDateFormat("dd:hh:mm:ss");
+        //Date resultdate = new Date(elapsedTime);
+        //System.out.println("Startzeit: " + sdf.format(new Date(startTimeMillis)));
+        //System.out.println(sdf.format(new Date(uptimeMillis)));
+        //send(sdf.format(resultdate));
+
     }
 }
--- src/bot/SplashBot.java
+++ src/bot/SplashBot.java
@@ -20,7 +20,7 @@ public class SplashBot extends PircBot {
     //public String channel = "#splshbot";
     public String auth = "oauth:bat48hhqj1bebsju7eigtxbvxcw2p8";
 
-    private Long startTimeMillis;
+    public Long startTimeMillis;
 
     public SplashBot() {
         this.setName("splshbot");
@@ -56,6 +56,37 @@ public class SplashBot extends PircBot {
         }
     }
 
+    /*public void getUptime(){
+      Long uptimeMillis = System.currentTimeMillis();
+
+        Long elapsedTime = uptimeMillis - this.startTimeMillis;
+
+        long timeInSeconds = elapsedTime / 1000;
+        long hours, minutes, seconds;
+        hours = timeInSeconds / 3600;
+        timeInSeconds = timeInSeconds - (hours * 3600);
+        minutes = timeInSeconds / 60;
+        timeInSeconds = timeInSeconds - (minutes * 60);
+        seconds = timeInSeconds;
+
+        String diffTime =
+                (hours<10 ? "0" + hours : hours) + " h "
+                        + (minutes < 10 ? "0" + minutes : minutes) + " min "
+                        + (seconds < 10 ? "0" + seconds : seconds) + " sec";
+
+
+        send(diffTime);
+
+        //String uptime = startTimeMillis.toString();
+        //SimpleDateFormat sdf = new SimpleDateFormat("dd:hh:mm:ss");
+        //Date resultdate = new Date(elapsedTime);
+        //System.out.println("Startzeit: " + sdf.format(new Date(startTimeMillis)));
+        //System.out.println(sdf.format(new Date(uptimeMillis)));
+        //send(sdf.format(resultdate));
+
+    }*/
+
+
     /**
      * Connect to the IRC server and saves the current time for the 'uptime' command.
      */
@@ -77,47 +108,19 @@ public class SplashBot extends PircBot {
         this.sendMessage(channel, message);
     }
 
-    private void getUptime(){
-        Long uptimeMillis = System.currentTimeMillis();
 
-        Long elapsedTime = uptimeMillis - startTimeMillis;
-
-        long timeInSeconds = elapsedTime / 1000;
-        long hours, minutes, seconds;
-        hours = timeInSeconds / 3600;
-        timeInSeconds = timeInSeconds - (hours * 3600);
-        minutes = timeInSeconds / 60;
-        timeInSeconds = timeInSeconds - (minutes * 60);
-        seconds = timeInSeconds;
-
-        String diffTime =
-                (hours<10 ? "0" + hours : hours) + " h "
-                + (minutes < 10 ? "0" + minutes : minutes) + " min "
-                + (seconds < 10 ? "0" + seconds : seconds) + " sec";
-
-
-        send(diffTime);
-
-        //String uptime = startTimeMillis.toString();
-        //SimpleDateFormat sdf = new SimpleDateFormat("dd:hh:mm:ss");
-        //Date resultdate = new Date(elapsedTime);
-        //System.out.println("Startzeit: " + sdf.format(new Date(startTimeMillis)));
-        //System.out.println(sdf.format(new Date(uptimeMillis)));
-        //send(sdf.format(resultdate));
-    }
 
     public boolean checkGrammar() throws IOException {
         controllgrammar.checkCommand();
         return controllgrammar.checkError();
     }
 
-    public void callMethods(String command) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+   public void callMethods(String command) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
-            Commandmethods methods = new Commandmethods();
+            Commandmethods methods = new Commandmethods(this);
             Class methodClass = methods.getClass();
 
-            Method method1 = methodClass.getMethod(command, new Class[]{});
-            method1.invoke(methods, new Object[]{});
-
+            Method method1 = methodClass.getMethod(command);
+            method1.invoke(methods);
     }
 }
